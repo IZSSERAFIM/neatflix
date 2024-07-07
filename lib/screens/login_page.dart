6a2b599df5073ef713879159e83736367a171d00
liruobin <king.alex.realmadrid@gmail.com>
2024-07-07 15:52:35 +0800

    fix some bugs in login and signup page

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:neatflix/screens/screens.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String username = "";
+  String password = "";
 
   @override
   Widget build(BuildContext context) {
@@ -38,49 +47,77 @@ class LoginPage extends StatelessWidget {
   }
 
   _inputField(context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        TextField(
-          decoration: InputDecoration(
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          TextFormField(
+            decoration: InputDecoration(
               hintText: "Username",
               border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(18),
                   borderSide: BorderSide.none),
               fillColor: Colors.purple.withOpacity(0.1),
               filled: true,
-              prefixIcon: const Icon(Icons.person)),
-        ),
-        const SizedBox(height: 10),
-        TextField(
-          decoration: InputDecoration(
-            hintText: "Password",
-            border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(18),
-                borderSide: BorderSide.none),
-            fillColor: Colors.purple.withOpacity(0.1),
-            filled: true,
-            prefixIcon: const Icon(Icons.password),
-          ),
-          obscureText: true,
-        ),
-        const SizedBox(height: 10),
-        ElevatedButton(
-          onPressed: () {},
-          style: ElevatedButton.styleFrom(
-            shape: const StadiumBorder(),
-            padding: const EdgeInsets.symmetric(vertical: 16),
-            backgroundColor: Colors.purple,
-          ),
-          child: const Text(
-            "Login",
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.white,
+              prefixIcon: const Icon(Icons.person),
             ),
+            onChanged: (value) {
+              setState(() {
+                username = value;
+              });
+            },
+            validator: (value) {
+              return value!.trim().isNotEmpty
+                  ? null
+                  : "username cannot be empty";
+            },
           ),
-        )
-      ],
+          const SizedBox(height: 10),
+          TextFormField(
+            decoration: InputDecoration(
+              hintText: "Password",
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(18),
+                  borderSide: BorderSide.none),
+              fillColor: Colors.purple.withOpacity(0.1),
+              filled: true,
+              prefixIcon: const Icon(Icons.password),
+            ),
+            obscureText: true,
+            onChanged: (value) {
+              setState(() {
+                password = value;
+              });
+            },
+            validator: (value) {
+              return value!.trim().isNotEmpty
+                  ? null
+                  : "password cannot be empty";
+            },
+          ),
+          const SizedBox(height: 10),
+          ElevatedButton(
+            onPressed: () {
+              if (_formKey.currentState!.validate()) {
+                print("username: $username, password: $password");
+              }
+            },
+            style: ElevatedButton.styleFrom(
+              shape: const StadiumBorder(),
+              padding: const EdgeInsets.symmetric(vertical: 16),
+              backgroundColor: Colors.purple,
+            ),
+            child: const Text(
+              "Login",
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 
@@ -100,18 +137,19 @@ class LoginPage extends StatelessWidget {
       children: [
         const Text("Dont have an account? "),
         TextButton(
-            onPressed: () {
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => SignupPage(),
-                ),
-              );
-            },
-            child: const Text(
-              "Sign Up",
-              style: TextStyle(color: Colors.purple),
-            ))
+          onPressed: () {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (context) => SignupPage(),
+              ),
+            );
+          },
+          child: const Text(
+            "Sign Up",
+            style: TextStyle(color: Colors.purple),
+          ),
+        )
       ],
     );
   }
--- lib/screens/signup_page.dart
+++ lib/screens/signup_page.dart
@@ -1,8 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:neatflix/screens/screens.dart';
 
-class SignupPage extends StatelessWidget {
+class SignupPage extends StatefulWidget {
   const SignupPage({super.key});
+
+  @override
+  State<SignupPage> createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String username = "";
+  String password = "";
+  String confirmPassword = "";
+  String email = "";
+
+  void _showPasswordMismatchDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Error"),
+          content: const Text("Passwords do not match. Please try again."),
+          actions: <Widget>[
+            TextButton(
+              child: const Text("OK"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,84 +45,139 @@ class SignupPage extends StatelessWidget {
             padding: const EdgeInsets.symmetric(horizontal: 40),
             height: MediaQuery.of(context).size.height - 50,
             width: double.infinity,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Column(
-                  children: <Widget>[
-                    const SizedBox(height: 60.0),
-                    const Text(
-                      "Sign up",
-                      style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold,
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      const SizedBox(height: 60.0),
+                      const Text(
+                        "Sign up",
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Text(
-                      "Create your account",
-                      style: TextStyle(fontSize: 15, color: Colors.grey[700]),
-                    )
-                  ],
-                ),
-                Column(
-                  children: <Widget>[
-                    TextField(
-                      decoration: InputDecoration(
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        "Create your account",
+                        style: TextStyle(fontSize: 15, color: Colors.grey[700]),
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      TextFormField(
+                        decoration: InputDecoration(
                           hintText: "Username",
                           border: OutlineInputBorder(
                               borderRadius: BorderRadius.circular(18),
                               borderSide: BorderSide.none),
                           fillColor: Colors.purple.withOpacity(0.1),
                           filled: true,
-                          prefixIcon: const Icon(Icons.person)),
-                    ),
-                    const SizedBox(height: 20),
-                    TextField(
-                      decoration: InputDecoration(
+                          prefixIcon: const Icon(Icons.person),
+                        ),
+                        onChanged: (value) {
+                          setState(() {
+                            username = value;
+                          });
+                        },
+                        validator: (value) {
+                          return value!.trim().isNotEmpty
+                              ? null
+                              : "username cannot be empty";
+                        },
+                      ),
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        decoration: InputDecoration(
                           hintText: "Email",
                           border: OutlineInputBorder(
                               borderRadius: BorderRadius.circular(18),
                               borderSide: BorderSide.none),
                           fillColor: Colors.purple.withOpacity(0.1),
                           filled: true,
-                          prefixIcon: const Icon(Icons.email)),
-                    ),
-                    const SizedBox(height: 20),
-                    TextField(
-                      decoration: InputDecoration(
-                        hintText: "Password",
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(18),
-                            borderSide: BorderSide.none),
-                        fillColor: Colors.purple.withOpacity(0.1),
-                        filled: true,
-                        prefixIcon: const Icon(Icons.password),
+                          prefixIcon: const Icon(Icons.email),
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+                        onChanged: (value) {
+                          setState(() {
+                            email = value;
+                          });
+                        },
+                        validator: (value) {
+                          return value!.trim().isNotEmpty
+                              ? null
+                              : "email cannot be empty";
+                        },
                       ),
-                      obscureText: true,
-                    ),
-                    const SizedBox(height: 20),
-                    TextField(
-                      decoration: InputDecoration(
-                        hintText: "Confirm Password",
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(18),
-                            borderSide: BorderSide.none),
-                        fillColor: Colors.purple.withOpacity(0.1),
-                        filled: true,
-                        prefixIcon: const Icon(Icons.password),
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          hintText: "Password",
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(18),
+                              borderSide: BorderSide.none),
+                          fillColor: Colors.purple.withOpacity(0.1),
+                          filled: true,
+                          prefixIcon: const Icon(Icons.password),
+                        ),
+                        obscureText: true,
+                        onChanged: (value) {
+                          setState(() {
+                            password = value;
+                          });
+                        },
+                        validator: (value) {
+                          return value!.trim().isNotEmpty
+                              ? null
+                              : "password cannot be empty";
+                        },
                       ),
-                      obscureText: true,
-                    ),
-                  ],
-                ),
-                Container(
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          hintText: "Confirm Password",
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(18),
+                              borderSide: BorderSide.none),
+                          fillColor: Colors.purple.withOpacity(0.1),
+                          filled: true,
+                          prefixIcon: const Icon(Icons.password),
+                        ),
+                        obscureText: true,
+                        onChanged: (value) {
+                          setState(() {
+                            confirmPassword = value;
+                          });
+                        },
+                        validator: (value) {
+                          return value!.trim().isNotEmpty
+                              ? null
+                              : "confirm password cannot be empty";
+                        },
+                      ),
+                    ],
+                  ),
+                  Container(
                     padding: const EdgeInsets.only(top: 3, left: 3),
                     child: ElevatedButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          if (password != confirmPassword) {
+                            _showPasswordMismatchDialog();
+                          } else {
+                            print(
+                                "username: $username, email: $email, password: $password");
+                            // Proceed with the sign-up process
+                          }
+                        }
+                      },
                       child: const Text(
                         "Sign up",
                         style: TextStyle(
@@ -104,12 +190,13 @@ class SignupPage extends StatelessWidget {
                         padding: const EdgeInsets.symmetric(vertical: 16),
                         backgroundColor: Colors.purple,
                       ),
-                    )),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    const Text("Already have an account?"),
-                    TextButton(
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      const Text("Already have an account?"),
+                      TextButton(
                         onPressed: () {
                           Navigator.pushReplacement(
                             context,
@@ -121,10 +208,12 @@ class SignupPage extends StatelessWidget {
                         child: const Text(
                           "Login",
                           style: TextStyle(color: Colors.purple),
-                        ))
-                  ],
-                )
-              ],
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ),
